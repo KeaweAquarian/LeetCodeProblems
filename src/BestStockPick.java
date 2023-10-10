a78cd6a1f5e627dc6d854ed9ea6b135b4f348e03
KeaweA <keaweaquarian@gmail.com>
2023-10-10 14:35:21 -0400

    New problem

--- src/BestStockPick.java
+++ src/BestStockPick.java
@@ -2,7 +2,7 @@ public class BestStockPick {
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
         System.out.println(maxProfit(prices));
-
+//lkzfgl;kadfmglkam
     }
     public static int maxProfit(int[] prices) {
      if(prices.length < 2)return -1;
