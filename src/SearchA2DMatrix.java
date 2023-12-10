23e79ca80d18e956f224fc1ac0e1179aedae5f78
KeaweA <keaweaquarian@gmail.com>
2023-12-10 18:25:50 -0500

    New problem

--- src/SearchA2DMatrix.java
+++ src/SearchA2DMatrix.java
@@ -1,6 +1,7 @@
 public class SearchA2DMatrix {
     public static void main(String[] args) {
 
+        //runs O(log(n*n))
         int [][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         int target = 3;
         System.out.println(searchMatrix(matrix, target));
